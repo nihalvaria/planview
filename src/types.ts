dad803b0128887c8e44f4a6c631e2896e38ea829
nihalvaria <nihalsinghvaria@gmail.com>
2022-07-03 02:47:39 +0530

    Added types for generateDiagram

--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,22 @@
-export type TGenerateDiagram = (a: number, b: number) => number;
+export type TPoint2D = {
+    x: number;
+    y: number;
+};
+
+export type TSectionsConfig = {
+    name?: string;
+    rotation?: number;
+    points: TPoint2D[];
+};
+
+export type TDiagramConfig = {
+    width: number;
+    height: number;
+    sections: TSectionsConfig[];
+};
+
+export type TDiagramResult = {
+    diagram: string;
+};
+
+export type TGenerateDiagram = (diagramConfig: TDiagramConfig) => TDiagramResult;
