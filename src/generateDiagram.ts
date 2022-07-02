5249828b1e3e52201e6d2ab80ba18ae59d205309
nihalvaria <nihalsinghvaria@gmail.com>
2022-07-03 02:48:47 +0530

    Updated generateDiagram, added types and canvas

--- src/generateDiagram.ts
+++ src/generateDiagram.ts
@@ -1,5 +1,14 @@
 import { TGenerateDiagram } from 'types';
 
-export const generateDiagram: TGenerateDiagram = (a, b) => {
-    return a + b;
+export const generateDiagram: TGenerateDiagram = diagramConfig => {
+    const canvas: HTMLCanvasElement = document.createElement('canvas');
+
+    canvas.width = diagramConfig.width || 100;
+    canvas.height = diagramConfig.height || 100;
+
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    ctx.rect(0, 0, canvas.width, canvas.height);
+
+    return { diagram: canvas.toDataURL() };
 };
