6f3a60d9781afb0b4a32249e0745495e879debf4
nihalvaria <nihalsinghvaria@gmail.com>
2022-07-03 17:27:20 +0530

    Fixed broken test cases for generate diagram

--- src/__test__/generateDiagram.test.ts
+++ src/__test__/generateDiagram.test.ts
@@ -1,7 +1,32 @@
 import { generateDiagram } from '../generateDiagram';
+import { TDiagramConfig } from '../types';
 
 describe('generateDiagram', () => {
     it('works', () => {
-        expect(generateDiagram(1, 1)).toEqual(2);
+        const relative_points: TDiagramConfig = {
+            width: 500,
+            height: 500,
+            backgroundColor: '#009dff',
+            sections: [
+                {
+                    name: 'Room 1',
+                    rotation: 75,
+                    backgroundColor: '#CFD8DC',
+                    points: [
+                        { x: 100, y: 340 },
+                        { x: 250, y: 340 },
+                        { x: 250, y: 150 },
+                        { x: 100, y: 150 },
+                        { x: 100, y: 340 },
+                    ],
+                },
+            ],
+        };
+
+        expect(generateDiagram(relative_points)).toEqual(
+            expect.objectContaining({
+                diagram: expect.any(String),
+            }),
+        );
     });
 });
