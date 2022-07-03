5eaea2ce66e6082be33c94a889063fe686b00cdc
nihalvaria <nihalsinghvaria@gmail.com>
2022-07-03 19:13:59 +0530

    Updated exports for fillColor

--- src/utils/fillColor.ts
+++ src/utils/fillColor.ts
@@ -1,6 +1,8 @@
-export const fillColor = (ctx: CanvasRenderingContext2D, color: string | undefined) => {
+const fillColor = (ctx: CanvasRenderingContext2D, color: string | undefined) => {
     if (color) {
         ctx.fillStyle = color;
         ctx.fill();
     }
 };
+
+export default fillColor;
