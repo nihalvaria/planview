b442f7ae61fca0e6b9b5fcdb21911a893b0e5389
nihalvaria <nihalsinghvaria@gmail.com>
2022-07-04 21:58:47 +0530

    fillColor moved to common util styling

--- src/utils/fillColor.ts
+++
@@ -1,8 +0,0 @@
-const fillColor = (ctx: CanvasRenderingContext2D, color: string | undefined) => {
-    if (color) {
-        ctx.fillStyle = color;
-        ctx.fill();
-    }
-};
-
-export default fillColor;
